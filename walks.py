42d79b94e2699aa2bff93163c69959c6231dbb95
Collin R Warner <collinw@mit.edu>
2022-02-10 00:15:32 -0500

    new math

--- walks.py
+++ walks.py
@@ -1,5 +1,7 @@
 import random as r
 import matplotlib.pyplot as plt
+import numpy as np
+import numpy.linalg as linalg
 
 def in_1d_boundary(position, boundary):
     return position > boundary[0] and position < boundary[1]
@@ -65,8 +67,72 @@ def run_sim(trials, steps, boundary):
     # post processing of results
     visualize_data(trial_results)
 
+
+def markov_processes():
+    n = 9 
+    m = np.zeros((n, n))
+    m[0, 1] = .5
+    m[n-1, n-2] = .5
+    for i in range(1, n-1):
+        m[i, i-1] = .5
+        m[i, i+1] = .5
+    print(m)
+    p_m = linalg.matrix_power(m, 10000)
+    np.set_printoptions(precision=4)
+    print(p_m)
+    v = np.zeros(n)
+    v[n//2] = 1
+    res = p_m@v
+    print(res)
+    print("end probability: ", res[n//2] )
+
+def out_before(s, b):
+    pos = 0
+    for c in s[:-1]:
+        if c == '0':
+            pos -= 1
+        else: 
+            pos += 1
+
+        if abs(pos) >= b:
+            return True
+
+    return False
+
+def out_at_end(s, b):
+    pos = 0
+    for c in s:
+        if c == '0':
+            pos -= 1
+        else:
+            pos += 1
+    return abs(pos) >= b
+
+def counting(s, b):
+    words = []
+    for i in range(2**s):
+        words.append(format(i, f"0{s}b"))
+    
+    #print(words)
+    valid_words = []
+    for w in words:
+        if not out_before(w, b):
+            valid_words.append(w)
+    
+    escaped_words = []
+    for w in valid_words:
+        if out_at_end(w, b):
+            escaped_words.append(w)
+    print(f"Valid Words {len(valid_words)}\n")#, valid_words)
+    print(f"Escaped Words {len(escaped_words)}\n")#, escaped_words) 
+    print(f"Probability Escaping on step {s}: ", len(escaped_words)/len(valid_words)) 
+
 if __name__ == "__main__":
-    b_1d = (float("-inf"), 7)
-    trials = 1
-    steps = 10
-    run_sim(trials, steps, b_1d)
+    steps = 20 
+    bound = 2
+    counting(steps, bound)
+    #markov_processes()
+    # b_1d = (float("-inf"), 7)
+    # trials = 1
+    # steps = 10
+    # run_sim(trials, steps, b_1d)
